8757d82a38b17741ed85d245eeaee1d6d23a135b
GaianiMagali <magaliantonella.gaiani@gmail.com>
2020-03-05 13:50:15 -0300

    listo

--- callback.js
+++ callback.js
@@ -5,3 +5,34 @@ function siguiente(x, next) {
 siguiente(2, resultado => { 
   console.log('Siguiente de 8 =>', resultado)
 })
+
+//--------------------------------
+function doble(x,next){
+  next(x*2)
+  };
+  
+  doble(8, resultado =>{
+  console.log('El doble de 8 =>', resultado);
+  });
+  
+  //--------------------------------
+  function calcular(resultado){
+  doble(resultado, res => console.log(`Siguiente del doble => ${res}`))
+  }
+  
+  // siguiente(2, resultado => {
+  // console.log('Siguiente de 2 =>', resultado)
+  // })
+  // doble(2, resultado => console.log('El doble de 2 =>', resultado));
+  
+  // doble(5,resultado => {
+  // siguiente(resultado , res => console.log('El doble del siguiente',res))
+  // })
+  
+  siguiente(2, calcular )
+  
+  //--------------------------------
+  function cuadruple(resultado){
+  doble(resultado,res => console.log(`El cuadruple de 8 es => ${res}`));
+  }
+  doble(8, cuadruple)
